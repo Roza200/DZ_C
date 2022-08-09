b4c2432379b89a05c265f9e6edd4e2716309c564
Roza Gaydash <g0401200@mail.ru>
2022-08-09 20:40:02 +0700

    Tasks

--- HomeWorks/HomeWork_1/Program.cs
+++ HomeWorks/HomeWork_1/Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Input first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine(" max " + number1 + " min " + number2);
+}
+else
+{
+    Console.WriteLine(" max " + number2 + " min " + number1);
+}
+
+*/
+
+ /*
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Input first number: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input second number: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input third number: ");
+int number_3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number_1;
+if (number_2 > max)
+{
+    max = number_2;
+}
+
+if (number_3 > max)
+{
+    max = number_3;
+}
+Console.Write(" max " + max);
+*/
+
+/*
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Input integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+  {
+   Console.WriteLine(" Yes ");
+  }
+else
+  {
+    Console.WriteLine(" NO ");
+  }
+*/
+
+/*
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Input integer positive number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = 2;
+
+while (current <= number)
+{
+  Console.Write( current  + " , ");
+  current++;
+  current++; 
+}  
+
+*/
+
+
+
