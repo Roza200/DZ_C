373bd209cc7635ac5d21713db9d460097e73ea3d
Roza Gaydash <g0401200@mail.ru>
2022-08-12 20:35:17 +0700

    Task 15 correction

--- HomeWorks/HomeWork_2/Program.cs
+++ HomeWorks/HomeWork_2/Program.cs
@@ -21,7 +21,7 @@ Console.WriteLine($"New version of {randomNumber} is {newNumber}");
 
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-
+/*
 void ThirtDigit(int number)
 {
     while (number >= 1000)
@@ -36,114 +36,33 @@ void ThirtDigit(int number)
        Console.WriteLine($" a third digit {number % 10} ");     
 } 
 
-Console.Write("Input numbur: ");
+Console.Write("Input number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 ThirtDigit(number);
+*/
 
 
-
-
-
-
-// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру этого числа
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 /*
-int FindBiggestDigit(int number)
+void DayWeek(int dayNumber)
 {
-    int ed = number % 10;
-    int dec = number / 10;
-    int max;
-
-    if(ed > dec)
-        max =  ed;
+    if (dayNumber >= 6)
+      Console.WriteLine($"This day {dayNumber} is a day off ");
+    
     else
-        max = dec;
-
-    return max;
-
+      Console.WriteLine($"This day {dayNumber} is not a day off ");   
 }
 
+Console.WriteLine("Input number of the day of the week: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
 
-int randomNumber = new Random().Next(10, 100);
+DayWeek(dayNumber);
 
-int beggestDigit = FindBiggestDigit(randomNumber);
-Console.WriteLine($"Beggest digit of {randomNumber} is {beggestDigit}");
 */
 
 
- // Напишите программу, которая выводит случайное 3х значное число и удаляет вторую цифру этого числа
-/*
-
- int CutNumber(int number)
- {
-    int ed = number % 10;
-    int sot = number / 100;
-
-    return  sot * 10 + ed;
-
- }
 
 
- int randomNumber = new Random().Next(100, 1000);
-
-int newNumber = CutNumber(randomNumber);
-Console.WriteLine($"New version of {randomNumber} is {newNumber}");
-*/
-
-// Задача 3. Напишите прорамму, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число не кратно, то праграмма находит остаток от деления
-
-/*
-void IsDivisionable(int num1, int num2)
-{
-    int reminder = num2 % num1;
-
-    if(reminder == 0)
-        Console.WriteLine($"{num2} is IsDivisionabl of {num1} ");
-    else
-       Console.WriteLine($"{num2} is not IsDivisionabl of {num1}. Reminder is {reminder} ");     
-
-}
 
 
-Console.WriteLine("Input ferst numbur: ");
-int n1 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Input second numbur: ");
-int n2 = Convert.ToInt32(Console.ReadLine());
-
-IsDivisionable(n1, n2);
-*/
-
-
-//Задача 4. Напишите программу, которая принимает на вход число и проверяет кратно ли оно одновременно 7 и 23.
-
-/*
-bool IsDivis(int number)
-{
-    if(number % 7  == 0 && number % 23 == 0)
-       return true;
-    else
-       return false;
-}
-
-Console.WriteLine("Input numbur: ");
-int n = Convert.ToInt32(Console.ReadLine());
-
-bool isDivision = IsDivis(n);
-Console.WriteLine(isDivision);
-*/
-
-// или записать так
-
-/*
-bool IsDivis(int number)
-{
-    return number % 7  == 0 && number % 23 == 0;  
-}
-
-Console.WriteLine("Input numbur: ");
-int n = Convert.ToInt32(Console.ReadLine());
-
-bool isDivision = IsDivis(n);
-Console.WriteLine(isDivision);
-*/
