3cf9e94b819ede5e185f1aa16374c49e28ebd1d9
Roza Gaydash <g0401200@mail.ru>
2022-08-22 12:18:12 +0700

    Task 27

--- HomeWorks/HomeWork_4/Program.cs
+++ HomeWorks/HomeWork_4/Program.cs
@@ -1,6 +1,5 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-//3, 5 -> 243 (3⁵)
-//2, 4 -> 16
+/*
 int DegreeNumber(int numberA,int numberB)
 {
    int result = 1;
@@ -19,7 +18,27 @@ int b = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine($"{a} to the power of {b} equals {DegreeNumber(a, b)} ");
 
+*/
 
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+/*
+int FindSum(int number)
+{ 
+   int sum = 0;
+   while (number > 0)
+   {
+    int num = number % 10;
+    number = number/10;
+    sum = sum + num;
+   }  
+   return sum;
+}
+
+Console.Write("Input integer number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Sum of all the numbers from {a} is {FindSum(a)}");
+*/
 
 
 //Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
